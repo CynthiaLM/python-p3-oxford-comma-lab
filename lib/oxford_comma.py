d2fd3fcdb64c48319e160872c57a4f1f3b79700a
Laleti Cynthia <+>
2023-10-16 09:14:48 +0300

    lab submission

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        first_part = ", ".join(items[:-1])
+        last_part = f", and {items[-1]}"
+        return f"{first_part}{last_part}"
+
